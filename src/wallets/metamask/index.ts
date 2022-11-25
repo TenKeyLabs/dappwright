38b1dd99c561501928d06cf50987d4c67d93025c
Dwayne Forde <dwayne@tenkeylabs.com>
2022-11-24 23:35:07 -0500

    cleans up commented hedge code

--- src/wallets/metamask/index.ts
+++ src/wallets/metamask/index.ts
@@ -47,39 +47,3 @@ export class MetaMask extends Wallet {
 
   static download = downloader(this.recommendedVersion);
 }
-// export const getMetmask = async (page: Page, version?: string): Promise<Dappwright> => {
-//   return {
-//     addNetwork: addNetwork(page, version),
-//     approve: approve(page, version),
-//     confirmTransaction: confirmTransaction(page, version),
-//     createAccount: createAccount(page, version),
-//     importPK: importPk(page, version),
-//     lock: lock(page, version),
-//     sign: sign(page, version),
-//     switchAccount: switchAccount(page, version),
-//     switchNetwork: switchNetwork(page, version),
-//     unlock: unlock(page, version),
-//     addToken: addToken(page),
-//     helpers: {
-//       getTokenBalance: getTokenBalance(page),
-//       deleteAccount: deleteAccount(page),
-//       deleteNetwork: deleteNetwork(page),
-//     },
-//     page,
-//   };
-// };
-
-/**
- * Return MetaMask instance
- * */
-// export async function getExtensionWindow(browser: Browser, version?: string): Promise<Dappwright> {
-//   const extensionPage = await new Promise<Page>((resolve) => {
-//     browser.contexts[0].pages().then((pages) => {
-//       for (const page of pages) {
-//         if (page.url().includes('chrome-extension')) resolve(page);
-//       }
-//     });
-//   });
-
-//   return getMetamask(extensionPage, version);
-// }
