9721e0a799361f1d3b81d76cac057c86b697aa30
Dwayne Forde <dwayne@tenkeylabs.com>
2023-01-17 17:23:35 -0500

    extension id extraction accounts for a different display format on linux chrome

--- src/wallets/wallets.ts
+++ src/wallets/wallets.ts
@@ -31,28 +31,29 @@ export const getWallet = async (id: WalletIdOptions, browserContext: BrowserCont
       // Wait for the wallet to pop up
       const page = await browserContext.waitForEvent('page', { timeout: 1000 });
       return new wallet(page);
-    } catch {
-      // Open the wallet manually if tab doesn't pop up
-      const page = await browserContext.newPage();
+    } catch {}
 
-      // Go to internal system page to list installed extensions
-      await page.goto('chrome://system/');
-      await page.waitForSelector('//button[@id="extensions-value-btn"]', { timeout: 5000 });
+    // Open the wallet manually if tab doesn't pop up
+    const page = await browserContext.newPage();
+
+    // Go to internal system page to list installed extensions
+    await page.goto('chrome://system/');
+
+    try {
+      await page.waitForSelector('//*[@id="extensions-value-btn"]', { timeout: 5000 });
       await page.getByRole('button', { name: 'Expand all…' }).click();
+    } catch {}
 
-      // Extract extension id
-      const extensionsEl = await page.waitForSelector('//*[@id="extensions-value"]');
-      const extensionsContext = (await extensionsEl.textContent()).split(/ : |\n/);
-      const walletNameIndex = extensionsContext.findIndex((context: string) =>
-        context.toLowerCase().includes(wallet.id),
-      );
-      const extensionId = extensionsContext[walletNameIndex - 1];
+    // Extract extension id
+    const extensionsEl = await page.waitForSelector('//*[@id="extensions-value"]');
+    const extensionsContext = (await extensionsEl.textContent()).split(/ : |\n/);
+    const walletNameIndex = extensionsContext.findIndex((context: string) => context.toLowerCase().includes(wallet.id));
+    const extensionId = extensionsContext[walletNameIndex - 1];
 
-      // Load extension homepage
-      await page.goto(`chrome-extension://${extensionId}${wallet.homePath}`);
+    // Load extension homepage
+    await page.goto(`chrome-extension://${extensionId}${wallet.homePath}`);
 
-      return new wallet(page);
-    }
+    return new wallet(page);
   }
 
   const page = browserContext.pages()[1];
