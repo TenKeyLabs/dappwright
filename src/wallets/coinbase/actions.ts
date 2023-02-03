b78173cab29c9006218420c5808cba32e291b043
Dwayne Forde <dwayne@tenkeylabs.com>
2023-02-02 22:08:25 -0500

    navigate home after coinbase setup

--- src/wallets/coinbase/actions.ts
+++ src/wallets/coinbase/actions.ts
@@ -8,6 +8,10 @@ const goHome = async (page: Page): Promise<void> => {
   await page.getByTestId('portfolio-navigation-link').click();
 };
 
+export const navigateHome = async (page: Page): Promise<void> => {
+  await page.goto(page.url().split('?')[0]);
+};
+
 export async function getStarted(
   page: Page,
   {
