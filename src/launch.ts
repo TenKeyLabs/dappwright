e9bfc2e24031c3ae3cdc215e712cd857d28b70ae
Artem Kuzmenko <artemk@defiyield.app>
2023-02-06 11:34:27 +0200

    fix: run headless

--- src/launch.ts
+++ src/launch.ts
@@ -17,7 +17,7 @@ export async function launch(browserName: string, options: OfficialOptions): Pro
   const extensionPath = await wallet.download(options);
 
   const browserContext = await playwright.chromium.launchPersistentContext(path.join(sessionPath, options.wallet), {
-    headless: false,
+    headless: true,
     args: [`--disable-extensions-except=${extensionPath}`, `--load-extension=${extensionPath}`],
   });
 
