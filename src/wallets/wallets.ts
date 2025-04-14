a3560a8977ea204df0789885e6b6ee567727f5aa
Dwayne Forde <347097+osis@users.noreply.github.com>
2025-04-13 20:58:56 -0400

    fix(metamask): race condition leads to acquiring dormant page context (#449)
    * fix(metamask): race condition leads to acquiring dormant page context
    
    * Create nine-mirrors-rest.md

--- src/wallets/wallets.ts
+++ src/wallets/wallets.ts
@@ -24,25 +24,26 @@ export const getWalletType = (id: WalletIdOptions): WalletTypes => {
   return walletType;
 };
 
+export const closeWalletSetupPopup = (id: WalletIdOptions, browserContext: BrowserContext): void => {
+  browserContext.on('page', async (page) => {
+    if (page.url() === walletHomeUrl(id)) {
+      await page.close();
+    }
+  });
+};
+
 export const getWallet = async (id: WalletIdOptions, browserContext: BrowserContext): Promise<MetaMaskWallet> => {
   const wallet = getWalletType(id);
+  const page = browserContext.pages()[0];
 
-  if (browserContext.pages().length === 1) {
-    let page: Page;
-    try {
-      // Wait for the wallet to pop up
-      page = await browserContext.waitForEvent('page', { timeout: 2000 });
-      await browserContext.pages()[0].close();
-      return new wallet(page);
-    } catch {
-      // Open the wallet manually if tab doesn't pop up
-      page = browserContext.pages()[0];
-      await page.goto(`chrome-extension://${EXTENSION_ID}${wallet.homePath}`);
-    }
-
-    return new wallet(page);
+  if (page.url() === 'about:blank') {
+    await page.goto(walletHomeUrl(id));
   }
 
-  const page = browserContext.pages()[0];
   return new wallet(page);
+};
+
+const walletHomeUrl = (id: WalletIdOptions): string => {
+  const wallet = getWalletType(id);
+  return `chrome-extension://${EXTENSION_ID}${wallet.homePath}`;
 };
