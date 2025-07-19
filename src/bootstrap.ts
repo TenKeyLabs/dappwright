d7d8c58b2938a9c9a253b86684e38ccb1dbb5cd6
Dwayne Forde <347097+osis@users.noreply.github.com>
2025-07-19 10:53:22 -0400

    fix: moves multi-tab listener before first getWallet call (#476)
    * chore: bumps metamask to 12.23.0
    
    * fix: moves multi-tab listener before first getWallet call
    
    * Create six-toes-try.md

--- src/bootstrap.ts
+++ src/bootstrap.ts
@@ -1,15 +1,14 @@
 import { BrowserContext, Page } from 'playwright-core';
 import { launch } from './launch';
 import { Dappwright, OfficialOptions } from './types';
-import { closeWalletSetupPopup, getWallet, WalletOptions } from './wallets/wallets';
+import { WalletOptions } from './wallets/wallets';
 
 export const bootstrap = async (
   browserName: string,
   { seed, password, showTestNets, ...launchOptions }: OfficialOptions & WalletOptions,
 ): Promise<[Dappwright, Page, BrowserContext]> => {
-  const { browserContext } = await launch(browserName, launchOptions);
-  closeWalletSetupPopup(launchOptions.wallet, browserContext);
-  const wallet = await getWallet(launchOptions.wallet, browserContext);
+  const { browserContext, wallet } = await launch(browserName, launchOptions);
+
   await wallet.setup({ seed, password, showTestNets });
 
   return [wallet, wallet.page, browserContext];
